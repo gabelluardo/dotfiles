91c330b0d6eec4f5fd50271129977a5c28ca2bbe
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2023-06-28 10:11:21 +0200

    rm: deno vr

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,5 @@
 # Source local environment variables file
 source ~/.fishenv 
 
-# velociraptor tab-completion
-source (vr completions fish | psub)
-
 # start starship prompt
 starship init fish | source
