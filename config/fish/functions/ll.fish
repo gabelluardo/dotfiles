198149aa4024b184e03d594b05fef3cbcc77ffee
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2023-09-07 16:36:21 +0200

    switch from `exa` to `eza` alias for ls

--- config/fish/functions/ll.fish
+++ config/fish/functions/ll.fish
@@ -1,4 +1,4 @@
-# Defined in - @ line 1
-function ll --wraps='exa -la' --description 'alias ll exa -la'
-  exa -la $argv;
+function ll --wraps=ls --wraps='eza -la' --description 'alias ll=eza -la'
+  eza -la $argv
+        
 end
