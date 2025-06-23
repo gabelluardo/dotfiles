c1895537b455f74dbbcf6e187011a7e45f0f57d4
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2025-06-23 17:41:49 +0200

    update

--- private_dot_config/fish/functions/btm.fish
+++ private_dot_config/fish/functions/btm.fish
@@ -1,4 +1,4 @@
-function btm --description 'alias btm=btm --theme gruvbox'
+function btm --description 'alias btm=btm --theme gruvbox --enable_gpu'
  command btm --theme gruvbox $argv
         
 end
